735ab0e5996d6dba0631c5c57873c03db8590736
Fernando R. Kurata <fernando.kurata@fdte.io>
2022-12-14 17:55:33 -0300

    Ajusting web mvc configuration.

--- rest_mvc_boilerplate-service/src/main/java/br/com/aqueteron/boilerplate/configuration/AppWebMvcConfigurer.java
+++ rest_mvc_boilerplate-service/src/main/java/br/com/aqueteron/boilerplate/configuration/AppWebMvcConfigurer.java
@@ -1,16 +1,25 @@
 package br.com.aqueteron.boilerplate.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
+@EnableWebMvc
 public class AppWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
                 .allowedHeaders("Accept", "Content-Type", "Authorization");
     }
--- rest_mvc_boilerplate-service/src/main/java/br/com/aqueteron/boilerplate/configuration/WebMvcSecurityConfiguration.java
+++ rest_mvc_boilerplate-service/src/main/java/br/com/aqueteron/boilerplate/configuration/WebMvcSecurityConfiguration.java
@@ -1,18 +1,23 @@
 package br.com.aqueteron.boilerplate.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class WebMvcSecurityConfiguration {
+public class WebMvcSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public DefaultSecurityFilterChain webHttpSecurity(final HttpSecurity http) throws Exception {
-        return http.build();
+    @Override
+    public void configure(final WebSecurity web) throws Exception {
+        web.ignoring().anyRequest();
     }
+
+//    @Bean
+//    public DefaultSecurityFilterChain webHttpSecurity(final HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll());
+//        return http.build();
+//    }
 
 }
